c65b772208beef7e743961db673787b2aa080a78
Jan-Lucas Uslu <janlucas.uslu@gmail.com>
2025-12-06 03:17:36 +0100

    updates

--- modeling/models/simple.py
+++ modeling/models/simple.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import SAGEConv, to_hetero
-from torch_geometric.data import HeteroData
+from torch_geometric.nn.conv import SAGEConv
 
 
 class GNN(torch.nn.Module):
@@ -23,6 +22,12 @@ class GNN(torch.nn.Module):
             aggr="mean",
             project=False,
         )
+        self.conv_3 = SAGEConv(
+            embedding_dim,
+            embedding_dim,
+            aggr="mean",
+            project=False,
+        )
 
     def forward(
         self,
@@ -30,50 +35,10 @@ class GNN(torch.nn.Module):
         edge_index: torch.Tensor,
     ) -> torch.Tensor:
         x = self.conv_1(x, edge_index)
+        x = F.relu(x)
+
         x = self.conv_2(x, edge_index)
+        x = F.relu(x)
+
+        x = self.conv_3(x, edge_index)
         return x
-
-
-class Model(torch.nn.Module):
-    def __init__(
-        self,
-        embedding_dim: int,
-    ):
-        super().__init__()
-
-        self.embedding_dim = embedding_dim
-
-        self.gnn = GNN(embedding_dim)
-
-    def forward(
-        self,
-        node_embeddings: torch.Tensor,
-        message_passing_edge_index: torch.Tensor,
-        supervision_edge_index: torch.Tensor,
-    ) -> torch.Tensor:
-
-        new_node_embeddings = self.gnn(
-            node_embeddings,
-            message_passing_edge_index,
-        )
-
-        # now compute dot product for each edge
-        src_node_ids = supervision_edge_index[0]
-        dst_node_ids = supervision_edge_index[1]
-
-        src_embeddings = new_node_embeddings[src_node_ids]
-        dst_embeddings = new_node_embeddings[dst_node_ids]
-        scores = (src_embeddings * dst_embeddings).sum(dim=1)
-        return scores
-
-    def get_node_embeddings(
-        self,
-        node_embeddings: torch.Tensor,
-        message_passing_edge_index: torch.Tensor,
-    ) -> torch.Tensor:
-
-        new_node_embeddings = self.gnn(
-            node_embeddings,
-            message_passing_edge_index,
-        )
-        return new_node_embeddings
