15286c8af3b3b1d2ce6b60f0a82c11f092dc662f
Jan-Lucas Uslu <janlucas.uslu@gmail.com>
2025-12-11 19:47:48 +0100

    added model

---
+++ modeling/models/simple_V2.py
@@ -0,0 +1,76 @@
+from torch_geometric.nn import to_hetero
+from torch_geometric.data import HeteroData
+import torch.nn.functional as F
+from torch_geometric.nn.conv import SAGEConv
+import torch
+
+
+class GNN(torch.nn.Module):
+    def __init__(
+        self,
+        embedding_dim: int,
+        num_layers: int,
+    ):
+        super().__init__()
+
+        self.convs = torch.nn.ModuleList(
+            [
+                SAGEConv(
+                    embedding_dim,
+                    embedding_dim,
+                    aggr="mean",
+                    project=True,
+                    normalize=True,
+                )
+                for _ in range(num_layers - 1)
+            ]
+        )
+
+        self.out_conv = SAGEConv(
+            embedding_dim,
+            embedding_dim,
+            aggr="mean",
+            project=False,
+        )
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        edge_index: torch.Tensor,
+    ) -> torch.Tensor:
+        for conv in self.convs:
+            x = conv(x, edge_index)
+            x = F.relu(x)
+
+        return self.out_conv(x, edge_index)
+
+
+class Model(torch.nn.Module):
+    def __init__(
+        self,
+        data: HeteroData,
+        embedding_dim: int = 256,
+        num_layers: int = 5,
+    ):
+        super().__init__()
+
+        self.embedding_dim = embedding_dim
+
+        self.gnn = GNN(embedding_dim, num_layers)
+        # Convert GNN model into a heterogeneous variant:
+        self.gnn = to_hetero(
+            self.gnn,
+            metadata=data.metadata(),
+            aggr="mean",
+        )
+
+    def forward(self, data: HeteroData) -> torch.Tensor:
+
+        x_dict = {
+            "author": data["author"].x,
+            "paper": data["paper"].x,
+        }
+
+        x_dict = self.gnn(x_dict, data.edge_index_dict)
+
+        return x_dict
