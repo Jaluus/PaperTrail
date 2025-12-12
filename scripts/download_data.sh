d28dae4e961aa367d9ce7e6f9272744958f9a6e9
jaluus <janlucas.uslu@gmail.com>
2025-12-11 20:48:42 -0800

    Update

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -3,12 +3,10 @@ echo "Downloading raw JSON data..."
 
 URL_BASE="https://papertraildata.s3.us-west-1.amazonaws.com/"
 FILENAMES=(
-    #"raw_data.pkl"
-    #"processed_data.pkl"
-    #"processed_normalized_data.pkl"
-    #"hetero_data.pt"
-    #"hetero_data_no_coauthor.pt"
-    "hetero_data_filtered_3_2.pt"
+    "raw_data.pkl"
+    "processed_data.pkl"
+    "processed_normalized_data.pkl"
+    "hetero_data.pt"
 )
 
 DATA_DIR=${1:-"./data"}
